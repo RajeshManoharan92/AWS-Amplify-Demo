321eff6df65bd00ddc70327da6d9aab15e47fe94
RajeshManoharan92 <m.rajeshmanohar@gmail.com>
2022-11-08 12:08:03 +0530

    AWS Amplify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet';
 
 function App() {
+  const position = [22.861029, 81.702662];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section>
+        <div>
+
+          <MapContainer center={position} zoom={5} scrollWheelZoom={true}>
+            
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[10.959406, 79.391265]}>
+              <Tooltip>My Home Town - Rajesh M</Tooltip>
+            </Marker>
+
+          </MapContainer>
+
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default App;
